7c7b3af7281b250f775a82857bf8f5decfe9e9cb
Jan Menzel <jan.menzel@seamlessinteraction.com>
2013-10-13 23:08:24 +0200

    battery function

--- js/mobile.js
+++ js/mobile.js
@@ -17,3 +17,19 @@ function vibrate(duration) {
 function notify(msg) {    
     var notification = new Notification("Notification", { body: msg });
 }
+
+// || navigator.mozBattery || navigator.webkitBattery;
+
+function setBatteryStatus() {
+    var battery = navigator.battery;
+    
+    var charging = battery.charging;
+    var level = battery.level * 100;
+    
+    console.log("Battery status: " + level + " %");
+    document.getElementById('battery').innerHTML = " " + level + " % - Charging: " + charging;
+    
+  if (battery.charging) {
+    console.log("Battery is charging"); 
+  }
+}
