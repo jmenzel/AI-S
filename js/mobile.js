2cf50780c60b88e5bbe6b1ea7b31c8f810005725
Jan Menzel <jan.menzel@seamlessinteraction.com>
2013-10-14 00:53:50 +0200

    neuer versuch

--- js/mobile.js
+++ js/mobile.js
@@ -60,18 +60,25 @@ function fail_suc() {
 
 
 if (window.DeviceOrientationEvent) {
-  window.addEventListener('deviceorientation', function(e) {
-    a = Math.floor(e.alpha);
-	b = Math.floor(e.beta);
-	g = Math.floor(e.gamma);
-	posUpdate(a,b,c);
- }, true);
+  //document.getElementById("doEvent").innerHTML = "DeviceOrientation";
+  // Listen for the deviceorientation event and handle the raw data
+  window.addEventListener('deviceorientation', function(eventData) {
+    // gamma is the left-to-right tilt in degrees, where right is positive
+    var tiltLR = eventData.gamma;
+
+    // beta is the front-to-back tilt in degrees, where front is positive
+    var tiltFB = eventData.beta;
+
+    // alpha is the compass direction the device is facing in degrees
+    var dir = eventData.alpha
+
+    // call our orientation event handler
+    deviceOrientationHandler(tiltLR, tiltFB, dir);
+  }, false);
+} else {
+  document.getElementById("orientation").innerHTML = "Device orientation - Not supported."
 }
 
-function posUpdate(a, b, c) {
-    elem = document.getElementById('orientation');
-    
-    elem.innerHTML = "X:" + a;
-    elem.innerHTML += "Y:" + b;
-    elem.innerHTML += "Z:" + c;
+function deviceOrientationHandler(a, b, c) {
+    document.getElementById('orientation').innerHTML = a+";"+b+";"+c;
 }
